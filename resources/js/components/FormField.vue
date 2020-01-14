48c459c4f51976c360124691ef5a84b7a31ef4f4
Gerard Lamusse <>
2020-01-14 20:15:13 +0100

    Add clear btn

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,34 +1,33 @@
 <template>
     <default-field :field="field" :errors="errors">
         <template slot="field">
-            <div>
-                <div v-if="value">
-                    <svg @click.prevent="zoom = !zoom" class="zoomer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12zM7 7V5h2v2h2v2H9v2H7V9H5V7h2z"/></svg>
-                    <CVideo v-if="value.indexOf('video/upload/') > -1"
-                        :id="field.name"
-                        :name="field.name"
-                        :src="value"
-                        class="block w-full card rounded cursor-pointer hoverableImage"
-                        :class="{zoom}"
-                        :title="zoom ? 'Zoom out' : 'Change Video'"
-                        @click.prevent="zoom ? zoom = false : openMediaLibrary()"></CVideo>
-                    <CImage
-                        v-else
-                        :id="field.name"
-                        :name="field.name"
-                        :src="value"
-                        class="block w-full card rounded cursor-pointer hoverableImage"
-                        :class="{zoom}"
-                        :title="zoom ? 'Zoom out' : 'Change Image'"
-                        @click.prevent="zoom ? zoom = false : openMediaLibrary()"
-                        alt="Cloudinary image url"
-                    />
-                    <div class="img-placeholder"></div>
-                </div>
-                <button v-else
-                    @click.prevent="openMediaLibrary"
-                    class="m-4 form-file-btn btn btn-default btn-primary select-none">Select</button>
+            <div v-if="value">
+                <svg @click.prevent="zoom = !zoom" class="zoomer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12zM7 7V5h2v2h2v2H9v2H7V9H5V7h2z"/></svg>
+                <button @click="update('')" class="p-1 mt-1 text-sm inline-block bg-danger text-white rounded">Clear</button>
+                <CVideo v-if="value.indexOf('video/upload/') > -1"
+                    :id="field.name"
+                    :name="field.name"
+                    :src="value"
+                    class="block w-full card rounded cursor-pointer hoverableImage"
+                    :class="{zoom}"
+                    :title="zoom ? 'Zoom out' : 'Change Video'"
+                    @click.prevent="zoom ? zoom = false : openMediaLibrary()"></CVideo>
+                <CImage
+                    v-else
+                    :id="field.name"
+                    :name="field.name"
+                    :src="value"
+                    class="block w-full card rounded cursor-pointer hoverableImage"
+                    :class="{zoom}"
+                    :title="zoom ? 'Zoom out' : 'Change Image'"
+                    @click.prevent="zoom ? zoom = false : openMediaLibrary()"
+                    alt="Cloudinary image url"
+                />
+                <div class="img-placeholder"></div>
             </div>
+            <button v-else
+                @click.prevent="openMediaLibrary"
+                class="m-4 form-file-btn btn btn-default btn-primary select-none">Select</button>
         </template>
     </default-field>
 </template>
