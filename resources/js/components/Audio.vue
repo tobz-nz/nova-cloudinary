8d72cdf85055a75a07eb144ba175e37174d96bd9
Toby Evans <tobz.nz@gmail.com>
2021-12-09 13:01:23 +1300

    Add support for audio files + previews on index page

---
+++ resources/js/components/Audio.vue
@@ -0,0 +1,24 @@
+<template>
+    <audio :src="src" preload="none" ref="caudio" @click="toggle" v-on:dblclick="reset" controls class="cloudinary-audio"></audio>
+</template>
+
+<script>
+export default {
+    props: ['src'],
+    methods: {
+        reset() {
+            const vid = this.$refs.caudio
+            vid.currentTime = 0
+            vid.play()
+        },
+
+        toggle() {
+            const vid = this.$refs.caudio
+            if (vid.paused)
+                vid.play()
+            else vid.pause()
+        }
+    }
+}
+</script>
+
--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,32 +1,46 @@
 <template>
-    <panel-item :field="field">
-        <template slot="value">
-            <CVideo v-if="field.value.resource_type === 'video'"
-                    :id="field.name"
-                    :name="field.name"
-                    :src="field.value.url"
-                    :width="field.value.width"
-                    :height="field.value.height"
-                    class="block w-full card rounded cursor-pointer hoverableImage"
-                    @click.prevent="openMediaLibrary"></CVideo>
-            <CImage
-                v-else
-                :id="field.name"
-                :name="field.name"
-                :src="field.value.url"
-                class="block w-full card rounded cursor-pointer hoverableImage"
-                @click.prevent="openMediaLibrary"
-                alt="Cloudinary image url"></CImage>
-        </template>
-    </panel-item>
+  <panel-item :field="field">
+    <template slot="value">
+      <CVideo
+        v-if="field.value.resource_type === 'video'"
+        :id="field.name"
+        :name="field.name"
+        :src="field.value.url"
+        :width="field.value.width"
+        :height="field.value.height"
+        class="block w-full card rounded cursor-pointer hoverableImage"
+        @click.prevent="openMediaLibrary"
+      ></CVideo>
+      <CAudio
+        v-else-if="field.value.is_audio === true"
+        :id="field.name"
+        :name="field.name"
+        :src="field.value.url"
+        :width="field.value.width"
+        :height="field.value.height"
+        class="block w-full card rounded cursor-pointer hoverableImage"
+        @click.prevent="openMediaLibrary"
+      ></CAudio>
+      <CImage
+        v-else
+        :id="field.name"
+        :name="field.name"
+        :src="field.value.url"
+        class="block w-full card rounded cursor-pointer hoverableImage"
+        @click.prevent="openMediaLibrary"
+        alt="Cloudinary image url"
+      ></CImage>
+    </template>
+  </panel-item>
 </template>
 
 <script>
-import CVideo from './Video.vue'
-import CImage from './Image.vue'
+import CVideo from "./Video.vue";
+import CAudio from "./Audio.vue";
+import CImage from "./Image.vue";
 
 export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
-    components: {CVideo, CImage},
-}
+  props: ["resource", "resourceName", "resourceId", "field"],
+  components: { CVideo, CAudio, CImage },
+};
 </script>
