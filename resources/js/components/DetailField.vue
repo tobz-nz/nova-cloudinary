26b43761ccb1190908c8aa5195ea42bfb40c3151
Toby Evans <tobz.nz@gmail.com>
2021-12-13 11:22:36 +1300

    Small tweak

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,35 +1,37 @@
 <template>
   <panel-item :field="field">
     <template v-if="field.value" slot="value">
-      <CAudio
-        v-if="field.value.is_audio === true"
-        :id="field.name"
-        :name="field.name"
-        :src="field.value.url"
-        :width="field.value.width"
-        :height="field.value.height"
-        class="block w-full card rounded cursor-pointer hoverableImage"
-        @click.prevent="openMediaLibrary"
-      ></CAudio>
-      <CVideo
-        v-else-if="field.value.resource_type === 'video'"
-        :id="field.name"
-        :name="field.name"
-        :src="field.value.url"
-        :width="field.value.width"
-        :height="field.value.height"
-        class="block w-full card rounded cursor-pointer hoverableImage"
-        @click.prevent="openMediaLibrary"
-      ></CVideo>
-      <CImage
-        v-else
-        :id="field.name"
-        :name="field.name"
-        :src="field.value.url"
-        class="block w-full card rounded cursor-pointer hoverableImage"
-        @click.prevent="openMediaLibrary"
-        alt="Cloudinary image url"
-      ></CImage>
+      <div class="cloudinary-field-container">
+          <CAudio
+            v-if="field.value.is_audio === true"
+            :id="field.name"
+            :name="field.name"
+            :src="field.value.url"
+            :width="field.value.width"
+            :height="field.value.height"
+            class="block w-full card rounded cursor-pointer hoverableImage"
+            @click.prevent="openMediaLibrary"
+          ></CAudio>
+          <CVideo
+            v-else-if="field.value.resource_type === 'video'"
+            :id="field.name"
+            :name="field.name"
+            :src="field.value.url"
+            :width="field.value.width"
+            :height="field.value.height"
+            class="block w-full card rounded cursor-pointer hoverableImage"
+            @click.prevent="openMediaLibrary"
+          ></CVideo>
+          <CImage
+            v-else
+            :id="field.name"
+            :name="field.name"
+            :src="field.value.url"
+            class="block w-full card rounded cursor-pointer hoverableImage"
+            @click.prevent="openMediaLibrary"
+            alt="Cloudinary image url"
+          ></CImage>
+      </div>
     </template>
   </panel-item>
 </template>
